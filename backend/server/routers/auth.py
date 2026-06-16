89ccc1c8de319850f9d59cc548b9d91acc41333a
Dhruv Pawar <dhruvpawar229@gmail.com>
2026-06-16 13:01:39 +0530

    login endpoint

--- backend/server/routers/auth.py
+++ backend/server/routers/auth.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.database import get_db
-from server.schemas.user import UserCreate, UserOutput
-from server.service.user import register_user
+from server.schemas.user import UserCreate, UserOutput, Token
+from server.service.user import register_user, login_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -11,3 +11,7 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post("/register", response_model=UserOutput, status_code=status.HTTP_201_CREATED)
 async def register(user_input: UserCreate, db: AsyncSession = Depends(get_db)):
     return await register_user(db, user_input)
+
+@router.post("/login",response_model=Token ,status_code=status.HTTP_200_OK)
+async def login(user_input: UserCreate, db: AsyncSession = Depends(get_db)):
+    return await login_user(db, user_input)
--- backend/server/schemas/user.py
+++ backend/server/schemas/user.py
@@ -5,14 +5,19 @@ from datetime import datetime
 class UserBase(BaseModel):
     """Base model for user."""
     email: EmailStr
-    display_name: Optional[str] = None
 
 class UserCreate(UserBase):
     """Create model for user."""
     password: str
+    display_name: Optional[str] = None
 
+class UserLogin(UserBase):
+    password: str
 class UserOutput(UserBase):
     id: uuid.UUID
     created_at: datetime
 
     model_config = {"from_attributes": True}
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
