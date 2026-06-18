dc6cc2e0c1da0b9fd34a948791723919fb92cd84
Dhruv Pawar <90130789+DhruvPawar22@users.noreply.github.com>
2026-06-18 12:40:41 +0530

    Feat/auth page (#36)
    * tailwind added
    
    * remove boilerplate
    
    * added inputfield
    
    * register e2e
    
    * login e2e
    
    * token handling
    
    * fixes
    
    * token handling complete #10

---
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import InputField from "../../components/InputField";
+import { Mail, Lock } from "lucide-react";
+import client from "../../api/client";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const {login} = useAuth();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    if (!email || !password) {
+      setError("Please fill in all fields");
+      return;
+    }
+
+
+    try {
+      const response = await client.post("/auth/login", { email, password });
+      login(response.data.access_token)
+      navigate("/dashboard");
+    } catch (err: any) {
+      setError(err.response?.data?.detail || "Something went wrong");
+    }
+  };
+
+  return (
+    <div className="auth-page">
+      <p className="auth-wordmark">bloom</p>
+
+      <h1 className="auth-heading">Welcome Back</h1>
+      <p className="auth-subtitle">Sign in to your Account</p>
+
+      <form onSubmit={handleSubmit} className="auth-form">
+        <InputField
+          type="email"
+          icon={<Mail size={16} />}
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <InputField
+          type="password"
+          icon={<Lock size={16} />}
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <p className="input-error">{error}</p>}
+
+        <button type="submit" className="btn-primary">
+          Login
+        </button>
+      </form>
+
+      <p className="auth-footer">
+        Don't have an account?{" "}
+        <Link to="/register" className="auth-link">Register</Link>
+      </p>
+    </div>
+  );
+};
+
+export default LoginPage;
